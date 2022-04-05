58fb5f42dad7933dc33c63acdc38361b547fa664
jonhbass <jb.mais@hotmail.com>
2022-04-05 13:03:14 -0300

    atualiz

--- Exercicios Python/ex001.py
+++ Exercicios Python/ex001.py
@@ -2,8 +2,8 @@ print('----------------')
 nome = input('digite seu nome:')
 import os
 print('Olá',nome,'muito prazer em te conhecer')
-n1 = float (input( 'digite o primeiro numero:'))
-n2 = float (input('digite o primeiro numero:'))
+n1 = int (input( 'digite o primeiro numero:'))
+n2 = int (input('digite o primeiro numero:'))
 total = n1 + n2 
-print('o total é',total)
+print('a soma entre {} e {} vale {}'.format(n1,n2,total))
 os.system('pause')
