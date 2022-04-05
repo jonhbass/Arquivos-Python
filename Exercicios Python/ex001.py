a5b00fde76ba0542cc8423832d99383aeb5502f1
jonhbass <jb.mais@hotmail.com>
2022-04-05 10:35:17 -0300

    ex001

---
+++ Exercicios Python/ex001.py
@@ -0,0 +1,9 @@
+print('----------------')
+nome = input('digite seu nome:')
+import os
+print('Olá',nome,'muito prazer em te conhecer')
+n1 = float (input( 'digite o primeiro numero:'))
+n2 = float (input('digite o primeiro numero:'))
+total = n1 + n2 
+print('o total é',total)
+os.system('pause')
